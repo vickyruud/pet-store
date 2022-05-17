9905fb06e452d19e177eefaf576be0024d4165d1
vickyruud <aprajit.k@gmail.com>
2022-05-17 01:26:50 -0400

    setup project and seed data

---
+++ db/migrate/20220517050119_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price, :precision => 8, :scale => 2
+      t.string :image
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
