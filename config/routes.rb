91ba1bb76a42ddc1ebed45de8c0fd027e6faa5bd
vickyruud <aprajit.k@gmail.com>
2022-06-08 20:30:06 -0400

    change image to image_url in seed data

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   # home page to show all products
   root to: 'products#index'
 
-  get '/show_deleted', to: 'products#show_deleted'
-  # put '/products/undelete/:id'=> 'products#undelete', :as => 'undelete_product'
-    
+  
+  get 'show_deleted', to: 'products#show_deleted'
   # product routes
   resources :products  do
     put 'undelete/:id', to: 'products#undelete', :as => 'undelete'
     get 'delete_comments/:id', to:'products#add_delete_comment_form', :as => 'delete_comment'
-
   end
 
 
